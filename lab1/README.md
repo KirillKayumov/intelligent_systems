f2c74feab5503ff7fb2e54f2a295d7bb344c8e2d
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-02-11 16:57:29 +0300

    Complete task 4

--- lab1/README.md
+++ lab1/README.md
@@ -2,7 +2,7 @@
 
 Исходный код для Задания 3 находится в файле **task3.rb**.
 
-Оценка производительности работы программы в Задании 2:
+### Оценка производительности работы программы в Задании 2:
 
 [:dirty, :dirty] | Points: 2
 
@@ -21,3 +21,50 @@
 [:clean, :clean] | Points: 0
 
 Average points: 1
+
+
+### Оценка производительности работы программы в Задании 4:
+
+[:dirty, :clean, :clean, :clean, :clean, :clean]
+
+[:clean, :clean, :clean, :clean, :clean, :dirty]
+
+[:dirty, :clean, :clean, :clean, :clean, :clean]
+
+[:clean, :clean, :clean, :dirty, :dirty, :dirty]
+
+[:clean, :dirty, :dirty, :dirty, :dirty, :clean]
+
+[:clean, :clean, :dirty, :dirty, :clean, :clean]
+
+[:clean, :clean, :clean, :clean, :clean, :dirty]
+
+Points: 13
+
+
+[:clean]
+
+[:clean]
+
+[:clean]
+
+[:dirty]
+
+[:clean]
+
+[:dirty]
+
+[:dirty]
+
+[:dirty]
+
+Points: 4
+
+
+[:clean, :dirty, :clean, :clean, :clean, :clean, :clean, :clean]
+
+[:dirty, :dirty, :dirty, :clean, :clean, :clean, :clean, :clean]
+
+[:clean, :clean, :clean, :clean, :dirty, :clean, :dirty, :clean]
+
+Points: 6
---
+++ lab1/task4.rb
@@ -0,0 +1,77 @@
+def rand_int(limit)
+  (1..limit).to_a.sample
+end
+
+class Agent # простой рефлексивный агент
+  def reflex(move_options, status)
+    move_options.select! { |_, value| value }
+    action = move_options.to_a.sample.first
+
+    if status == :dirty
+      :suck
+    else
+      action
+    end
+  end
+end
+
+class Environment # описание среды
+  attr_reader :map, :map_width, :map_height
+
+  AGENT_LIFE_TIME = 1000 # время работы агента
+
+  def initialize
+    @map_height, @map_width = rand_int(10), rand_int(10)
+    @map = []
+
+    map_height.times do
+      @map << []
+      map_width.times do
+        @map.last << (rand_int(100) % 3 == 0 ? :dirty : :clean)
+      end
+    end
+  end
+
+  def run
+    agent = Agent.new
+    points = 0 # очки, набранные агентом
+    location = [0, 0]
+
+    AGENT_LIFE_TIME.times do |time|
+      move_options = {
+        :up => location.first > 0,
+        :right => location.last < map_width - 1,
+        :down => location.first < map_height - 1,
+        :left => location.last > 0
+      }
+
+      action = agent.reflex(move_options, map[location.first][location.last])
+
+      if action == :suck
+        map[location.first][location.last] = :clean
+        points += 1 # прибавляем очки за очищенные клетки
+      elsif action == :up
+        location[0] -= 1
+      elsif action == :right
+        location[1] += 1
+      elsif action == :down
+        location[0] += 1
+      elsif action == :left
+        location[1] -= 1
+      end
+    end
+
+    points # возвращаем кол-во набранных очков
+  end
+end
+
+environment = Environment.new
+
+environment.map.each do |row|
+  print row
+  puts
+end
+
+points = environment.run
+
+puts "Points: #{points}"
